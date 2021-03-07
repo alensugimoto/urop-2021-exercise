3dc973e81381cd2dccf789548f6cb91cfda39fff
Alen Sugimoto <alensugimoto@gmail.com>
2021-03-07 11:22:29 +0100

    Update genetic algorithm inputs

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-from ga.genetic_algorithm import genetic_algorithm, decode
 from ga.chromosome_elem import ChromosomeElem
+from ga.genetic_algorithm import genetic_algorithm, filter
 from track_generator.command import Command
 from track_generator.generator import generate_track
+from config import CHROMOSOME_LENGTH
 
 import matplotlib.pyplot as plt
-from config import *
 
 if __name__ == '__main__':
 
+    num_bits_per_inst = 18
+    chromosome_length = CHROMOSOME_LENGTH
+    population_size = 100
+    num_generations = 100
+    penalty_coefs = (50.0, 0.0)
+    value_bounds = ((5.0, 10.0), (5.0, 10.0), (5.0, 10.0), (10.0, 45.0))
+    crossover_rate = 0.9
+    mutation_rate = 1.0 / float(num_bits_per_inst * chromosome_length)
+
     # perform the genetic algorithm search
-    best, score = genetic_algorithm(INST_VALUE_BOUNDS, CHROMOSOME_LENGTH, NUM_ELEMENT_BITS, NUM_GENERATIONS, POPULATION_SIZE, PENALTY_COEF, CROSSOVER_RATE, MUTATION_RATE)
-    print('Done!')
-    chromosome_elements = decode(INST_VALUE_BOUNDS, CHROMOSOME_LENGTH, NUM_ELEMENT_BITS, best)
-    print('f(%s) = %f' % ([str(d) for d in chromosome_elements], score))
+    chromosome_elements = genetic_algorithm(
+        num_bits_per_inst=num_bits_per_inst,
+        chromosome_length=chromosome_length,
+        population_size=population_size,
+        num_generations=num_generations,
+        penalty_coefs=penalty_coefs,
+        value_bounds=value_bounds,
+        crossover_rate=crossover_rate,
+        mutation_rate=mutation_rate,
+    )
 
     track_points = generate_track(chromosome_elements=chromosome_elements)
 
